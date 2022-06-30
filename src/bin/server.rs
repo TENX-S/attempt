d9ad7c257697b85c6e829db5109c28a6d4415ab8
TENX-S <ttenx@pm.me>
2022-06-30 11:23:40 +0800

    even further

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,3 @@
-use tonic::codegen::http::request;
 use tonic::{transport::Server, Request, Response, Status};
 
 use hello_world::greeter_server::{Greeter, GreeterServer};
@@ -18,10 +17,8 @@ impl Greeter for MyGreeter {
         request: Request<HelloRequest>,
     ) -> Result<Response<HelloReply>, Status> {
         println!("Got a request from {:?}", request.remote_addr());
-        let age = request.into_inner().age;
-        println!("{}", age);
         let reply = hello_world::HelloReply {
-            message: format!("Hello {}!", age),
+            message: format!("Hello {}!", request.into_inner().name),
         };
         Ok(Response::new(reply))
     }
